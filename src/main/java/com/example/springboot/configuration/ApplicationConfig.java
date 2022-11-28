1f3696a73251e4596e265dc4cf3cd05ceceebabb
sanglq3 <sanglq3@vng.com.vn>
2022-11-29 00:16:53 +0700

    Add simple jwt authentication -Login api -Hardcode user credentials for test -Without authorities

---
+++ src/main/java/com/example/springboot/configuration/ApplicationConfig.java
@@ -0,0 +1,27 @@
+package com.example.springboot.configuration;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApplicationConfig {
+    @Value("${prefix}")
+    private String prefixApi;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedMethods("POST", "PUT", "DELETE", "PATCH", "HEAD", "OPTIONS", "CONNECT", "TRACE");
+            }
+        };
+    }
+}
---
+++ src/main/java/com/example/springboot/dto/UserDto.java
@@ -0,0 +1,60 @@
+package com.example.springboot.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto implements UserDetails {
+    private Long id;
+    private String username;
+    private String email;
+    private String phoneNumber;
+    Collection<? extends GrantedAuthority> roles;
+
+    @JsonIgnore
+    private Integer isDeleted;
+
+    @JsonIgnore
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @JsonIgnore
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
+
