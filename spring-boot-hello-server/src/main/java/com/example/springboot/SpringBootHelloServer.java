4bda3621f3abd58428c40e332be2aeaba203d270
sanglq3 <sanglq3@vng.com.vn>
2022-12-03 16:07:43 +0700

    Update maven project structure

---
+++ spring-boot-hello-server/src/main/java/com/example/springboot/SpringBootHelloServer.java
@@ -0,0 +1,27 @@
+package com.example.springboot;
+
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * Disable autoconfiguration datasource, and we will be config in configuration package
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class SpringBootHelloServer{
+    public static Logger _Logger = LogManager.getLogger(SpringBootHelloServer.class);
+
+    public static void main(String[] args) {
+        try {
+            ConfigurableApplicationContext context = SpringApplication.run(SpringBootHelloServer.class, args);
+            _Logger.info("Server is running");
+        } catch (Exception ex) {
+            _Logger.error(ex.getMessage(), ex);
+        }
+    }
+}
