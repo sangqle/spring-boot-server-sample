50b213d2d7c776f6a958021981ef885e7f07da90
sanglq3 <sanglq3@vng.com.vn>
2022-12-04 01:52:32 +0700

    add simple file upload

--- spring-boot-file-upload/src/main/java/com/example/springboot/SpringBootServerApplication.java
+++ spring-boot-file-upload/src/main/java/com/example/springboot/SpringBootServerApplication.java
@@ -1,12 +1,15 @@
 package com.example.springboot;
 
 
+import com.example.springboot.storage.StorageService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Disable autoconfiguration datasource, and we will be config in configuration package
@@ -22,5 +25,13 @@ public class SpringBootServerApplication {
         } catch (Exception ex) {
             _Logger.error(ex.getMessage(), ex);
         }
+    }
+
+    @Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
     }
 }
--- spring-boot-file-upload/src/main/java/com/example/springboot/controller/UploadController.java
+++ spring-boot-file-upload/src/main/java/com/example/springboot/controller/UploadController.java
@@ -1,17 +1,36 @@
 package com.example.springboot.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.springboot.storage.FileSystemStorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequestMapping("/api/")
 @RestController
 public class UploadController {
+
+    @Autowired
+    FileSystemStorageService storageService;
+
+    @GetMapping("/upload/files")
+    public String listUploadedFiles() throws IOException {
+
+        List<String> filePaths =
+                storageService.loadAll()
+                        .map(path ->  path.getFileName().toString())
+                        .collect(Collectors.toList());
+
+        return filePaths.toString();
+    }
+
     @PostMapping("/upload")
-    public String handleFileUpload(@RequestParam("file")MultipartFile file) {
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) throws Exception {
         System.err.println(file.getOriginalFilename());
+        storageService.store(file);
         return "uploaded";
     }
 }
